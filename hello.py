eb9da7b31c431a26698bcd33a859ce1911d9614d
Adarsh Singh <adarsh.singh@example.com>
2026-05-25 20:09:05 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,33 @@
+# arithmetic operators
+
+# a = 23
+# b = 34
+# sum = a + b
+# difference = a - b
+# product = a * b
+# quotient = a / b
+# remainder = a % b
+
+# print("Sum:", sum)
+# print("Difference:", difference)
+# print("Product:", product)
+# print("Quotient:", quotient)
+# # print("Remainder:", remainder)
+
+# relational operators
+# a = 10
+# b = 20
+
+# print( a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+
+# assignment operators
+num =10
+num += 5
+num -=3
+num *=2
+num /=3
+num **=5
+print(num)
